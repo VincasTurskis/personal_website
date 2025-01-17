5b844b900ab578cd6af09f1b0f10397d5dd1f348
VincasTurskis <vincas.turskis@gmail.com>
2025-01-17 22:59:17 +0100

    Changed UI layout, added XSS sanitation with DOMPurify

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -2,44 +2,41 @@
 
 import React from 'react';
 import Styles from './styles.module.css';
+import DOMPurify from 'dompurify';
 
 export default function Contact()
 {
-    function handleSubmit(formData)
+    async function handleSubmit(formData)
     {
         const requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ 
-                name: formData.get("personName"),
-                email: formData.get("email"),
-                body: formData.get("body")
+                name: DOMPurify.sanitize(formData.get("personName")),
+                email: DOMPurify.sanitize(formData.get("email")),
+                body: DOMPurify.sanitize(formData.get("body"))
             })
         };
-        fetch("http://localhost:8080/sendEmail", requestOptions)
-        .then((response) => (console.log("Sent the request")))
-        console.log("You have submitted data\n:",
-            `Name: ${formData.get("personName")}\n`,
-            `Email: ${formData.get("email")}\n`,
-            `Message: ${formData.get("body")}`);
+        await fetch("http://localhost:8080/sendEmail", requestOptions)
+        .then((_) => (console.log("Sent the request")))
     }
     return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center">
-            <div>
-                <h1>Contact Vincas Turskis</h1>
-            </div>
-            <div className="row-start-2">
-                <form action={handleSubmit}>
+        <div className="h-screen bg-green-50 grid grid-rows-[20px_1fr_20px] grid-cols-[20px_1fr_20px] items-center justify-items-center">
+            <div className="col-start-2 row-start-2">
+                <form action={handleSubmit} name="contactForm">
+                    <h1 className="text-5xl font-bold">Get In Touch:</h1>
+                    <br />
                     <p><label htmlFor='personName'>Your name:</label></p>
-                    <input className={Styles.inputField} type='text' id='personName' name='personName' size='10' />
+                    <input required className={Styles.inputField} type='text' id='personName' name='personName' size='10' />
                     <br />
                     <br />
                     <p><label htmlFor='emailField'>Your email address:</label></p>
-                    <input className={Styles.inputField} type='email' id='email' name='email' size='10' />
+                    <input required className={Styles.inputField} type='email' id='email' name='email' size='10' />
                     <br />
                     <br />
                     <p><label htmlFor='body'>Your message:</label></p>
-                    <textarea className={Styles.inputField} id="body" name="body"></textarea>
+                    <textarea required className={Styles.inputField} id="body" name="body"></textarea>
+                    <br />
                     <br />
                     <input className={Styles.button} type="submit" value="Submit"></input>
                 </form>
