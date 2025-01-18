a33c89c1d63febeeee9e672149c93031c8fec7e6
VincasTurskis <vincas.turskis@gmail.com>
2025-01-18 13:23:40 +0100

    Added toast to form submit

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -3,9 +3,16 @@
 import React from 'react';
 import Styles from './styles.module.css';
 import DOMPurify from 'dompurify';
+import Snackbar from '@mui/material/Snackbar';
+import SnackbarContent from '@mui/material/SnackbarContent';
+
 
 export default function Contact()
 {
+    const [openSuccess, setOpenSuccess] = React.useState(false);
+    const [openFail, setOpenFail] = React.useState(false);
+    const vertical = 'top'
+    const horizontal = 'right';
     async function handleSubmit(formData)
     {
         const requestOptions = {
@@ -18,28 +25,77 @@ export default function Contact()
             })
         };
         await fetch("http://localhost:8080/sendEmail", requestOptions)
-        .then((_) => (console.log("Sent the request")))
+            .then((response) => {
+                if(response.ok)
+                {
+                    setOpenSuccess(true);
+                }
+                else
+                {
+                    setOpenFail(true);
+                }
+            });
+    }
+    function handleToastClose()
+    {
+        setOpenSuccess(false);
+        setOpenFail(false);
     }
     return (
-        <div className="min-h-screen bg-green-50 grid grid-rows-[20px_1fr_20px] grid-cols-[20px_1fr_20px] items-center justify-items-center">
-            <div className="col-start-2 row-start-2">
-                <form action={handleSubmit} name="contactForm">
-                    <h1 className="text-5xl font-bold">Get In Touch:</h1>
-                    <br />
-                    <p><label htmlFor='personName'>Your name:</label></p>
-                    <input required className={Styles.inputField} type='text' id='personName' name='personName' size='10' />
-                    <br />
-                    <br />
-                    <p><label htmlFor='emailField'>Your email address:</label></p>
-                    <input required className={Styles.inputField} type='email' id='email' name='email' size='10' />
-                    <br />
-                    <br />
-                    <p><label htmlFor='body'>Your message:</label></p>
-                    <textarea required className={Styles.inputField} id="body" name="body"></textarea>
-                    <br />
-                    <br />
-                    <input className={Styles.button} type="submit" value="Submit"></input>
-                </form>
+        <div>
+            <Snackbar
+                anchorOrigin={{ vertical, horizontal }}
+                autoHideDuration={4000}
+                open = {openSuccess}
+                onClose={handleToastClose}
+                key="toastSuccess"
+            >
+                <SnackbarContent style = {{
+                    backgroundColor: '#15803d',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}
+                message = {<p>Form submitted successfully!</p>}
+                />
+            </Snackbar>
+
+            <Snackbar
+                anchorOrigin={{ vertical, horizontal }}
+                autoHideDuration={8000}
+                open = {openFail}
+                onClose={handleToastClose}
+                key="toastFail"
+            >
+                <SnackbarContent style = {{
+                    backgroundColor: '#dc2626',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}
+                message = {<p>Something went wrong</p>}
+                />
+            </Snackbar>
+            <div className="min-h-screen bg-green-50 grid grid-rows-[20px_1fr_20px] grid-cols-[20px_1fr_20px] items-center justify-items-center">
+                <div className="col-start-2 row-start-2">
+                    <form action={handleSubmit} name="contactForm">
+                        <h1 className="text-5xl font-bold">Get In Touch:</h1>
+                        <br />
+                        <p><label htmlFor='personName'>Your name:</label></p>
+                        <input required className={Styles.inputField} type='text' id='personName' name='personName' size='10' />
+                        <br />
+                        <br />
+                        <p><label htmlFor='emailField'>Your email address:</label></p>
+                        <input required className={Styles.inputField} type='email' id='email' name='email' size='10' />
+                        <br />
+                        <br />
+                        <p><label htmlFor='body'>Your message:</label></p>
+                        <textarea required className={Styles.inputField} id="body" name="body"></textarea>
+                        <br />
+                        <br />
+                        <input className={Styles.button} type="submit" value="Submit"></input>
+                    </form>
+                </div>
             </div>
         </div>
     )
